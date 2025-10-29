ccb3f288e6151394c660a5c158c38d684b1cb5e5
Khoasinhnam2003 <21082761.khoa@student.iuh.edu.vn>
2025-10-29 16:34:34 +0700

    update config apk

--- foodapp-mobile/app.config.js
+++ foodapp-mobile/app.config.js
@@ -5,8 +5,38 @@ export default {
     name: "FoodApp Mobile",
     slug: "foodapp-mobile",
     scheme: "foodapp",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      package: "com.foodapp.mobile",
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      edgeToEdgeEnabled: true
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    plugins: [
+      "expo-secure-store"
+    ],
     extra: {
-      apiBaseUrl: process.env.API_BASE_URL
+      apiBaseUrl: process.env.API_BASE_URL,
+      eas: {
+        projectId: "855c7f93-21ad-4d97-af70-5c25888403b1"
+      }
     }
   }
 }
